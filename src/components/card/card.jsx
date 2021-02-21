48e9687678a64f8ac5864f4c97a983aba9b64915
ejrdnjs <ejrdnjs96@gmail.com>
2021-02-22 01:40:56 +0900

    Add Color Thema

--- src/components/card/card.jsx
+++ src/components/card/card.jsx
@@ -1,8 +1,17 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import styles from './card.module.css';
 
 const Card = ({card}) => {
-    return <div className={styles.card}>
+    const cardRef=useRef(null);
+
+    useEffect(()=>{
+        if(card.thema===null){
+            cardRef.current.style.backgroundColor=`white`;
+            return;
+        }
+        cardRef.current.style.backgroundImage=`url(${card.thema})`;
+    },[card])
+    return <div className={styles.card} ref={cardRef}>
         <img src={card.imgURL || "/images/default_logo.png"} alt="userImg" className={styles.userImg}/>
         <div className={styles.cardInfo}>
             <h2 className={styles.cardName}>{card.name}</h2>
